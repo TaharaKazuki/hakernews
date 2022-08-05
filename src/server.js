cf1b0dacf19098eb4865c255c4d9d5b6d0a975b4
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-08-05 16:24:51 +0900

    update

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Query {
+    info: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    info: () => 'HackerNewsクローン',
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  ${url}でサーバーを起動中`)
+})
