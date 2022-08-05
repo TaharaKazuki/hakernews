69cbb99b997180e8f97ea29166849e165133f3e7
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-08-05 17:00:16 +0900

    update

--- src/server.js
+++ src/server.js
@@ -1,14 +1,30 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+let links = [
+  {
+    id: 'link-0',
+    description: 'GraphQlのチュートリアル',
+    url: 'www.udemy',
+  },
+]
+
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
 const resolvers = {
   Query: {
     info: () => 'HackerNewsクローン',
+    feed: () => links,
   },
 }
 
